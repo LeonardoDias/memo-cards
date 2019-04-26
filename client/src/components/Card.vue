6d925cbf8c8f16c407b7e9292e4a44ac1f5d8edf
Leonardo Dias <leonardodias914@gmail.com>
2019-04-26 16:34:28 -0300

    Add Card component and CardViewer view

---
+++ client/src/components/Card.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card-container" ontouchstart="">
+    <div class="flipper">
+        <div class="card-front">
+            <p> Front </p>
+        </div>
+        <div class="card-back">
+            <p> Back </p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style scoped>
+  .card-container {
+    width: 100%;
+    height: 100%;
+    border: 1px solid #000;
+    margin: 0;
+    padding: 0;
+  }
+
+    .card-container:hover .flipper {
+      transform: rotateY(180deg);
+    }
+
+  .flipper {
+    transition: 1s;
+    transform-style: preserve-3d;
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
+  .card-front, .card-back {
+    width: 100%;
+    height: 100%;
+    background-color: beige;
+    backface-visibility: hidden;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .card-front {
+    z-index: 2;
+    transform: rotateY(0deg);
+  }
+
+  .card-back {
+    transform: rotateY(180deg);
+  }
+</style>
+
+
---
+++ client/src/views/CardViewer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="card-viewer">
+    <div class="card-component">
+      <Card/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+  name: 'CardViewer',
+  components: {
+    Card
+  },
+  props: {
+    cardHeight: Number,
+    cardWidth: Number
+  },
+  data: () => {
+    return {
+      cardHeightValue: 0,
+      cardWidthValue: 0
+    }
+  },
+  mounted() {
+    this.cardHeightValue = this.cardHeight;
+    this.cardWidthValue = this.cardWidth;
+    
+    const cardComponent = document.getElementsByClassName("card-component")[0];
+    cardComponent.style.width = `${this.cardWidthValue}px`;
+    cardComponent.style.height = `${this.cardHeightValue}px`;
+  },
+  watch: {
+    cardWidthValue: (newWidth, oldWitdh) => {
+      const cardComponent = document.getElementsByClassName("card-component")[0];
+      cardComponent.style.width = `${newWidth}px`;
+    },
+
+    cardHeightValue: (newHeight, oldHeight) => {
+      const cardComponent = document.getElementsByClassName("card-component")[0];
+      cardComponent.style.height = `${newHeight}px`;
+    },
+  }
+}
+</script>
+
+<style>
+  .card-viewer {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    align-content: center;
+    justify-content: center;
+  }
+
+  
+</style>
